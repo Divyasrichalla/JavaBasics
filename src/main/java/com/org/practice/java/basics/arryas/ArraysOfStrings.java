cf282cb7d92774b3de52adfb7d99dd4fc6242394
Divya sri <challadivyasri94@gmail.com>
2022-04-24 16:38:23 +0400

    arrays

---
+++ src/main/java/com/org/practice/java/basics/arryas/ArraysOfStrings.java
@@ -0,0 +1,18 @@
+package com.org.practice.java.basics.arryas;
+
+public class ArraysOfStrings {
+	public static void main(String[] args) {
+		String[] names = {"Div","Nav","Sri","Vena"};
+		for(String name: names){
+			System.out.println(name);
+		}
+		String[][] words = { {"Div","Nav","Sri","Vena"},{"413","407"},{"ECE","2012","2015"}};
+		for(int i=0;i< words.length;i++){
+			for(int j=0;j< words[i].length;j++){
+				System.out.print(words[i][j] +" ");
+			}
+			System.out.println();
+		}
+
+	}
+}
---
+++ src/main/java/com/org/practice/java/basics/arryas/PracticeArrays.java
@@ -0,0 +1,18 @@
+package com.org.practice.java.basics.arryas;
+
+public class PracticeArrays {
+	public static void main(String[] args) {
+		int[] numbers = {3,4,5,6,7};
+		for(int i=0;i<numbers.length;i++){
+			System.out.println(numbers[i]);
+		}
+		double[] doubleNums = {22.98,9722.23,123.12};
+		for(int i=0;i<doubleNums.length;i++){
+			System.out.println(doubleNums[i]);
+		}
+		char[] characters = {'d','n','s','v'};
+		for(int i=0;i<characters.length;i++){
+			System.out.println(characters[i]);
+		}
+	}
+}
